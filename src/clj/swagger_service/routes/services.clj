37efef7157b55a5e8c0cfd56018c03511f16b569
Ethan C. Eldridge <git@ethaneldridge.com>
2017-08-13 20:37:17 -0700

    toolbar support

--- src/clj/swagger_service/routes/services.clj
+++ src/clj/swagger_service/routes/services.clj
@@ -23,7 +23,6 @@
     (str response)))
 
 ;; ECE 2017.08.09 - https://github.com/metosin/compojure-api
-
 (s/defschema Rectangle
   {:width s/Int
    :height s/Int})
@@ -36,16 +35,19 @@
   {:id s/Str
    :name s/Str
    :imageName s/Str
-   :isVisible s/Bool
    :stackName s/Str
    :dimension Rectangle
    :locationOld {:salvoMapId s/Str
                  :salvoPoint Point}
    :locationNew {:salvoMapId s/Str
                  :salvoPoint Point
-                 }}
-  )
+                 }})
 
+(s/defschema LeftDoubleClick
+  {
+   :targetType (s/enum :MAP :PALETTE)
+   :targetId s/Str
+   :salvoPoint Point})
 
 (defapi service-routes
   {:swagger {:ui "/swagger-ui"
@@ -57,67 +59,49 @@
     :tags ["Vassal"] ;; ECE 2017.08.09
 
     (GET "/map" []
-;;      :return       json;;String ;;s/Str
       :query-params [id :- s/Str]
       :summary      "Get the map with the id provided on the query parameter"
       (->
        (send-request "localhost" 3030  (str "{\"GET_SALVOMAP_BY_ID\": {\"queryParameters\": { \"id\": \"" id "\"}}}"))
        ok
-       (content-type "application/json")
-       )
-      )
+       (content-type "application/json")))
 
     (GET "/gamestate" []
       :summary      "Get the complete game state"
       (->
        (send-request "localhost" 3030  (str "{\"GET_GAMESTATE\": {}}"))
        ok
-       (content-type "application/json")
-       )
-      )
+       (content-type "application/json")))
+
+    (GET "/toolbarmenu" []
+      :summary      "Get the toolbar menu"
+      (->
+       (send-request "localhost" 3030  (str "{\"GET_TOOLBAR_MENU\": {}}"))
+       ok
+       (content-type "application/json")))
 
     (POST "/gamepiece" []
       :body-params [gamePiece :- GamePiece]
       :summary     "Move a GamePiece from locationOld to locationNew"
       (let [gamePieceStr (json/write-str gamePiece)]
         (->
-         ;;       (send-request "localhost" 3030  (str "{\"POST_PIECE\": " "{}" "}"))
-         ;;       (send-request "localhost" 3030  (clojure.string/replace (str "{\"POST_PIECE\": " (json/write-str gamePiece) "}") #"\n" ""))
-         ;;       (send-request "localhost" 3030  (str "{\"GET_GAMESTATE\": {}}"))
-         (send-request "localhost" 3030 (str "{\"POST_PIECE\": " gamePieceStr "}"))
-
-         ;;       (clojure.string/replace (str "{\"POST_PIECE\": " (json/write-str gamePiece) "}") #"\n" "")
+          (send-request "localhost" 3030 (str "{\"POST_PIECE\": " gamePieceStr "}"))
          ok
-         (content-type "application/json")
-         ))
-      )
+         (content-type "application/json"))))
 
-    (GET "/plus" []
-      :return       Long
-      :query-params [x :- Long, {y :- Long 1}]
-      :summary      "x+y with query-parameters. y defaults to 1."
-      (ok (+ x y)))
+    (POST "/leftdoubleclick" []
+      :body-params [leftDoubleClick :- LeftDoubleClick]
+      :summary     "Left Double-Click the mouse on a target element"
+      (let [leftDoubleClickStr (json/write-str leftDoubleClick)]
+        (->
+          (send-request "localhost" 3030 (str "{\"POST_LEFT_DOUBLE_CLICK\": " leftDoubleClickStr "}"))
+         ok
+         (content-type "application/json"))))
 
-    (POST "/minus" []
-      :return      Long
-      :body-params [x :- Long, y :- Long]
-      :summary     "x-y with body-parameters."
-      (ok (- x y)))
-
-    (GET "/times/:x/:y" []
-      :return      Long
-      :path-params [x :- Long, y :- Long]
-      :summary     "x*y with path-parameters"
-      (ok (* x y)))
-
-    (POST "/divide" []
-      :return      Double
-      :form-params [x :- Long, y :- Long]
-      :summary     "x/y with form-parameters"
-      (ok (/ x y)))
-
-    (GET "/power" []
-      :return      Long
-      :header-params [x :- Long, y :- Long]
-      :summary     "x^y with header-parameters"
-      (ok (long (Math/pow x y))))))
+    (POST "/turntracker" []
+      :summary     "Advance the turn-tracker"
+      (->
+       (send-request "localhost" 3030 (str "{\"POST_TURNTRACKER\": {}}"))
+       ok
+       (content-type "application/json"))))
+  )
